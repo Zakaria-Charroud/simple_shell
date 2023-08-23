10a1a155dd0e0e64e45465d2aff435f0bf5debe4
Zakaria-Charroud <126858108+Zakaria-Charroud@users.noreply.github.com>
2023-08-23 08:31:49 +0200

    added

--- en1.c
+++ en1.c
@@ -10,17 +10,15 @@
  */
 int cmp_env_name(const char *nenv, const char *name)
 {
-	int i;
-
-	for (i = 0; nenv[i] != '='; i++)
-	{
-		if (nenv[i] != name[i])
-		{
-			return (0);
-		}
-	}
-
-	return (i + 1);
+int i;
+for (i = 0; nenv[i] != '='; i++)
+{
+if (nenv[i] != name[i])
+{
+return (0);
+}
+}
+return (i + 1);
 }
 
 /**
@@ -33,26 +31,24 @@ int cmp_env_name(const char *nenv, const char *name)
  */
 char *_getenv(const char *name, char **_environ)
 {
-	char *ptr_env;
-	int i, mov;
-
-	/* Initialize ptr_env value */
-	ptr_env = NULL;
-	mov = 0;
-	/* Compare all environment variables */
-	/* environ is declared in the header file */
-	for (i = 0; _environ[i]; i++)
-	{
-		/* If name and env are equal */
-		mov = cmp_env_name(_environ[i], name);
-		if (mov)
-		{
-			ptr_env = _environ[i];
-			break;
-		}
-	}
-
-	return (ptr_env + mov);
+char *ptr_env;
+int i, mov;
+/* Initialize ptr_env value */
+ptr_env = NULL;
+mov = 0;
+/* Compare all environment variables */
+/* environ is declared in the header file */
+for (i = 0; _environ[i]; i++)
+{
+/* If name and env are equal */
+mov = cmp_env_name(_environ[i], name);
+if (mov)
+{
+ptr_env = _environ[i];
+break;
+}
+}
+return (ptr_env + mov);
 }
 
 /**
@@ -63,18 +59,14 @@ char *_getenv(const char *name, char **_environ)
  */
 int _env(data_shell *datash)
 {
-	int i, j;
-
-	for (i = 0; datash->_environ[i]; i++)
-	{
-
-		for (j = 0; datash->_environ[i][j]; j++)
-			;
-
-		write(STDOUT_FILENO, datash->_environ[i], j);
-		write(STDOUT_FILENO, "\n", 1);
-	}
-	datash->status = 0;
-
-	return (1);
+int i, j;
+for (i = 0; datash->_environ[i]; i++)
+{
+for (j = 0; datash->_environ[i][j]; j++)
+;
+write(STDOUT_FILENO, datash->_environ[i], j);
+write(STDOUT_FILENO, "\n", 1);
+}
+datash->status = 0;
+return (1);
 }
